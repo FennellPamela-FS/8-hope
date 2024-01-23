7f0418d87dd6b2feb52ee21658a5565659112970
FennellPamela-FS <pyfennell@student.fullsail.edu>
2024-01-22 23:46:42 -0500

    Connect to DB

--- dev/project/server.js
+++ dev/project/server.js
@@ -1,10 +1,21 @@
 // Set up a basic Express server
-require("dotenv").config('.env')
+
 const express = require('express');
 const app = express();
+const cors = require('cors');
+require("dotenv").config('.env')
 const port = process.env.PORT || 5000;
+
+app.use(cors())
+app.use(express.json())
 
 app.listen(port, () =>
     console.log(`Server running on port ${port}`)
 );
 
+// connect to database
+const mongoose = require('mongoose');
+mongoose.connect(process.env.MONGODB_URI);
+if (mongoose) {
+    console.log("Successfully connected to MongoDB.");
+}
