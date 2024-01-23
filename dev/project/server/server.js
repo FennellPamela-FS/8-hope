b8ef605c5017a08bbe571a63c39039b71904b88c
FennellPamela-FS <pyfennell@student.fullsail.edu>
2024-01-23 13:33:44 -0500

    Update server, db conn

--- dev/project/server/server.js
+++ dev/project/server/server.js
@@ -1,4 +1,10 @@
 // Set up a basic Express server
+// Backend Server
+
+const http = require('http');
+// const app = require('./app/app');
+// const port = process.env.PORT || 5000;
+// require('dotenv').config();
 
 const express = require('express');
 const app = express();
@@ -9,13 +15,33 @@ const port = process.env.PORT || 5000;
 app.use(cors())
 app.use(express.json())
 
-app.listen(port, () =>
-    console.log(`Server running on port ${port}`)
-);
 
-// connect to database
-const mongoose = require('mongoose');
-mongoose.connect(process.env.MONGODB_URI);
-if (mongoose) {
-    console.log("Successfully connected to MongoDB.");
-}
+// get driver connection
+const dbo = require('./db/conn')
+
+// app.listen(port, () =>
+//     console.log(`Server running on port ${port}`)
+// );
+
+// app.listen(port, () => {
+//     // connect to database when server starts
+//     dbo.connectToServer(function (err) {
+//         if (err) console.error(err);
+//     })
+//     console.log(`Backend is running on port: ${port}`)
+// });
+
+
+
+// request listener
+http.createServer(app).listen(port, () => {
+    console.log(`Server running on ${port}`);
+});
+
+
+// // connect to database
+// const mongoose = require('mongoose');
+// mongoose.connect(process.env.MONGODB_URI);
+// if (mongoose) {
+//     console.log("Successfully connected to MongoDB.");
+// }
