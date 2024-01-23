a14beba647c9c7d7ca676e5737ddfa84d308781d
FennellPamela-FS <pyfennell@student.fullsail.edu>
2024-01-23 06:07:05 -0500

    Update structure

--- dev/project/server/server.js
+++ dev/project/server/server.js
@@ -3,7 +3,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-require("dotenv").config('.env')
+require("dotenv").config('./env')
 const port = process.env.PORT || 5000;
 
 app.use(cors())
