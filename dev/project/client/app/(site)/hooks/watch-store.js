eee099c4c9321a60b8bf266639af1fc39bd749be
FennellPamela-FS <pyfennell@student.fullsail.edu>
2024-02-01 21:20:31 -0500

    Update watchStore with prayerWatch times

--- dev/project/client/app/(site)/hooks/watch-store.js
+++ dev/project/client/app/(site)/hooks/watch-store.js
@@ -9,7 +9,15 @@ import { create } from 'zustand'
 
 export const useWatchStore = create(set => ({
     // state variable
-    prayerWatches: [],
+    prayerWatches: [
+        '6:00 PM - 9:00 PM',
+        '9:00 PM - 12:00 AM',
+        '12:00 AM - 3:00 AM',
+        '3:00 AM - 6:00 AM',
+        '6:00 AM - 9:00 AM',
+        '9:00 AM - 12:00 PM',
+        '12:00 PM - 3:00 PM',
+    ],
     // action that updates the state variable
     setPrayerWatches: (watches) => set({ prayerWatches: watches }),
 }));
