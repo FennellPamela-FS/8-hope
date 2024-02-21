f06918fb1dd46f9a242b660513035440abc48182
FennellPamela-FS <pyfennell@student.fullsail.edu>
2024-02-21 07:07:34 -0500

    make model and API route for PrayerWatches

---
+++ dev/project/server/api/prayerWatches.js
@@ -0,0 +1,19 @@
+// api/prayerWatches.js (on the server)
+const dbConn = require('../db/conn');
+const { PrayerWatch } = require('../db/models/PrayerWatch');
+
+const handler = async (req, res) => {   // returns a Promise
+    try {
+        await dbConn.sync();    //confirm db connection
+
+        const prayerWatches = await PrayerWatch.find({});   // fetches all prayer watches from the database
+        res.status(200).json({ success: true, data: prayerWatches });
+
+    } catch (error) {
+        res.status(500).json({ success: false, message: error.message });
+    }
+};
+
+module.exports = {
+    handler,
+}
---
+++ dev/project/server/db/models/PrayerWatch.js
@@ -0,0 +1,11 @@
+// models/PrayerWatches.js (on the server)
+const mongoose = require('mongoose');
+
+const prayerWatchSchema = new mongoose.Schema({
+    title: String,
+    timeRange: String,
+    description: String,
+    scriptureReference: String,
+});
+
+module.exports = mongoose.model('PrayerWatch', prayerWatchSchema);
