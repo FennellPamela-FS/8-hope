45019172f73c1380e1c6875d5e7c57956ab8152e
FennellPamela-FS <pyfennell@student.fullsail.edu>
2024-02-21 20:16:23 -0500

    Update to add api endpoint

--- dev/project/client/components/PrayerWatchTimesList.js
+++ dev/project/client/components/PrayerWatchTimesList.js
@@ -1,22 +1,44 @@
 'use client'
 // components/PrayerWatchTimesList.js
-import React from 'react';
-import styles from '../app/page.module.css';
 
+import React, { useEffect } from 'react';
+import styles from '../app/page.module.css';
 import { usePrayerWatchTimesStore } from '../stores/prayerWatchTimesStore';
 import { Box, Button, Typography } from '@mui/material';
-
-// import Link from '@mui/material/Link';
 import { Link as NextLink } from 'next/link';
 
 const PrayerWatchTimesList = () => {
 
-    const { prayerWatchTimes } = usePrayerWatchTimesStore();
+    const { prayerWatchTimes, setPrayerWatches } = usePrayerWatchTimesStore();
     // Define your prayer watches data here or import it from another file
+
+    const apiPrayerWatches = 'http://localhost:8000/api/prayerWatches';
+
+    const fetchPrayerWatches = async () => {
+        try {
+            const res = await fetch(`${apiPrayerWatches}`);
+            const data = await res.json();
+            setPrayerWatches(data); // Update the prayerWatchTimes state with the fetched data
+            console.log(data); // Log the data object to the console for debugging purposes
+        } catch (error) {
+            console.error('Failed to fetch prayer watch', error);
+        }
+    };
+
+    useEffect(() => {
+        fetchPrayerWatches(); // Fetch prayer watches on component mount
+        // console.log('prayerWatchTimes', prayerWatchTimes); // Log the prayerWatchTimes state for debugging purposes
+        // console.log('setPrayerWatches', setPrayerWatches); // Log the setPrayerWatches state for debugging purposes
+        // return () => {
+        //     setPrayerWatches({}); // Clear the prayerWatchTimes state on component unmount
+        // };  // Clean up the prayerWatchTimes state on component unmount
+        // // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [setPrayerWatches]);
 
     return (
         <div>
 
+            {/* use Object.entries to iterate over key-value pairs in prayerWatchTimes */}
             {Object.entries(prayerWatchTimes).map(([timeRange, routePath]) => (
                 <Box key={routePath} sx={{ color: 'blue', display: 'block' }}>
                     <Button className={styles.avBox} sx={{ mb: 2 }} variant="contained" component={NextLink} href={`/${routePath}`}>
@@ -34,14 +56,13 @@ const PrayerWatchTimesList = () => {
 
 export default PrayerWatchTimesList;
 
-{/* <Box
-    className={styles.avBox}
-    key={index}
-    sx={{ my: 2, color: 'blue', display: 'block' }}
->
-    <Button sx={{ mb: 2 }} variant="contained" component={NextLink} href="/">
-        <Typography variant='h6'>
-            {timeRange}
-        </Typography>
-    </Button>
-</Box> */}
+
+// useEffect(() => {
+//     const fetchPrayerWatches = async () => {
+//         const res = await fetch('/api/prayerWatches');
+//         const data = await res.json();
+//         setPrayerWatchTimes(data); // Update the prayerWatchTimes state with the fetched data
+//         console.log(data); // Log the data object to the console for debugging purposes
+//     };
+//     fetchPrayerWatches();
+// }, [setPrayerWatches])
