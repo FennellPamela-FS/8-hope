aed1b39d47ade37c756c9ec279a699a89e47edfc
FennellPamela-FS <pyfennell@student.fullsail.edu>
2024-02-21 20:17:35 -0500

    Update store

--- dev/project/client/stores/prayerWatchTimesStore.js
+++ dev/project/client/stores/prayerWatchTimesStore.js
@@ -5,7 +5,8 @@ export const usePrayerWatchTimesStore = create(set => ({
     prayerWatchTimes:
     {
         // state variable
-        // ... your prayer watch times here
+        // prayer watch times in the following format
+        // timeRange: 'routePath',
         '6:00 PM - 9:00 PM': 'watch-one',
         '9:00 PM - 12:00 AM': 'watch-two',
         '12:00 AM - 3:00 AM': 'watch-three',
@@ -15,26 +16,7 @@ export const usePrayerWatchTimesStore = create(set => ({
         '12:00 PM - 3:00 PM': 'watch-seven',
         '3:00 PM - 6:00 PM': 'watch-eight',
     },
-    // [{
-    //     '6:00 PM - 9:00 PM': 'First Watch',
-    //     '9:00 PM - 12:00 AM': 'Second Watch',
-    //     '12:00 AM - 3:00 AM': 'Third Watch',
-    //     '3:00 AM - 6:00 AM': 'Fourth Watch',
-    //     '6:00 AM - 9:00 AM': 'Fifth Watch',
-    //     '9:00 AM - 12:00 PM': 'Sixth Watch',
-    //     '12:00 PM - 3:00 PM': 'Seventh Watch',
-    //     '3:00 PM - 6:00 PM': 'Eighth Watch',
-    // }],
 
     // action that updates the state variable
     setPrayerWatches: (watches) => set({ prayerWatches: watches }),
 }));
-
-
-// const useBearStore = create((set) => ({
-//     bears: 0,
-//     increasePopulation: () => set((state) => ({ bears: state.bears + 1 })),
-//     removeAllBears: () => set({ bears: 0 }),
-// }))
-
-
