930e3785cab67465d70b6ea422d03587c40c243e
FennellPamela-FS <pyfennell@student.fullsail.edu>
2024-02-25 13:50:46 -0500

    Fix merge conflicts

--- dev/project/client/components/PrayerWatchTimesList.js
+++ dev/project/client/components/PrayerWatchTimesList.js
@@ -14,22 +14,9 @@ const PrayerWatchTimesList = () => {
 
     return (
         <div>
-<<<<<<< HEAD
-
-            {prayerWatchTimes.map((timeRange, index) => (
-                // <li key={index}>{timeRange}</li>
-
-                <Box
-                    // className={styles.avBox}
-                    key={index}
-                    sx={{ maxWidth: 'sm', color: 'blue', display: 'block' }}
-                >
-                    <Button sx={{ mb: 2 }} variant="contained" component={NextLink} href={`/${prayerWatchRoutes[timeRange]}`}>
-=======
             {prayerWatchTimes.map((watch) => (
                 <Box key={watch.link} sx={{ color: 'blue', display: 'block' }}>
                     <Button className={styles.avBox} sx={{ mb: 2 }} variant="contained" component={NextLink} href={`/${watch.link}`}>
->>>>>>> models_shemas
                         <Typography variant='h6'>
                             {watch.timeRange}
                         </Typography>
